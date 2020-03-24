75aaf3993e0d274dc736f5104904b07b9ba60e61
Xiaolin <xlbian@thoughtworks.com>
2020-03-24 11:28:47 +0800

    Add the tests for product Backstage pass. Over the time, the quality has increased by 1, 2, or 3. But after the sellIn goes down to -1, the quality need to be 0.

--- src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
@@ -9,13 +9,30 @@ public class GildedRose {
 
   public void updateQuality() {
     for (int i = 0; i < items.length; i++) {
-      if(items[i].name != "Sulfures"){
+      if (items[i].name != "Sulfures") {
         items[i].sellIn = items[i].sellIn - 1;
 
-        if(items[i].name == "Aged Brie"){
-          if(items[i].quality < 50){
+        if (items[i].name == "Aged Brie") {
+          if (items[i].quality < 50) {
             items[i].quality = items[i].quality + 1;
           }
+        } else if (items[i].name == "Backstage pass") {
+          if (items[i].sellIn < 0) {
+            items[i].quality = 0;
+          } else {
+            if (items[i].quality < 50) {
+              if (items[i].sellIn > 10) {
+                items[i].quality = items[i].quality + 1;
+              } else if (items[i].sellIn <= 10 && items[i].sellIn > 5) {
+                items[i].quality = items[i].quality + 2;
+              } else {
+                items[i].quality = items[i].quality + 3;
+              }
+            } else {
+              items[i].quality = 50;
+            }
+          }
+
         } else {
           if (items[i].sellIn >= 0) {
             if (items[i].quality > 0) {
@@ -28,8 +45,6 @@ public class GildedRose {
           }
         }
       }
-
-
 
     }
   }
--- src/test/java/cn/xpbootcamp/gilded_rose/GildedRoseTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/GildedRoseTest.java
@@ -95,4 +95,79 @@ class GildedRoseTest {
     assertEquals(2, app.items[0].sellIn);
     assertEquals(40, app.items[0].quality);
   }
+
+  @Test
+  void backstagePassQualityOneDaysLater(){
+    Item[] items = new Item[] { new Item("Backstage pass", 12, 25) };
+    GildedRose app = new GildedRose(items);
+
+    int days = 1;
+    for (int i = 0; i < days; i++) {
+      app.updateQuality();
+    }
+
+    assertEquals("Backstage pass", app.items[0].name);
+    assertEquals(11, app.items[0].sellIn);
+    assertEquals(26, app.items[0].quality);
+  }
+
+  @Test
+  void backstagePassQualityTwoDaysLater(){
+    Item[] items = new Item[] { new Item("Backstage pass", 12, 25) };
+    GildedRose app = new GildedRose(items);
+
+    int days = 2;
+    for (int i = 0; i < days; i++) {
+      app.updateQuality();
+    }
+
+    assertEquals("Backstage pass", app.items[0].name);
+    assertEquals(10, app.items[0].sellIn);
+    assertEquals(28, app.items[0].quality);
+  }
+
+  @Test
+  void backstagePassQualitySevenDaysLater(){
+    Item[] items = new Item[] { new Item("Backstage pass", 12, 25) };
+    GildedRose app = new GildedRose(items);
+
+    int days = 7;
+    for (int i = 0; i < days; i++) {
+      app.updateQuality();
+    }
+
+    assertEquals("Backstage pass", app.items[0].name);
+    assertEquals(5, app.items[0].sellIn);
+    assertEquals(39, app.items[0].quality);
+  }
+
+  @Test
+  void backstagePassQualityTwelveDaysLater(){
+    Item[] items = new Item[] { new Item("Backstage pass", 12, 25) };
+    GildedRose app = new GildedRose(items);
+
+    int days = 12;
+    for (int i = 0; i < days; i++) {
+      app.updateQuality();
+    }
+
+    assertEquals("Backstage pass", app.items[0].name);
+    assertEquals(0, app.items[0].sellIn);
+    assertEquals(50, app.items[0].quality);
+  }
+
+  @Test
+  void backstagePassQualityThirteenDaysLater(){
+    Item[] items = new Item[] { new Item("Backstage pass", 12, 25) };
+    GildedRose app = new GildedRose(items);
+
+    int days = 13;
+    for (int i = 0; i < days; i++) {
+      app.updateQuality();
+    }
+
+    assertEquals("Backstage pass", app.items[0].name);
+    assertEquals(-1, app.items[0].sellIn);
+    assertEquals(0, app.items[0].quality);
+  }
 }
